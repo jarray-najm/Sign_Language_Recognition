4d669ee580fc9eb04f0827b38bf33017290c7901
Najmeddine <najmejarray@gmail.com>
2024-02-10 22:30:46 +0100

    Project v 1.0.0

---
+++ Code/CameraHandSign.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import time
+import numpy as np
+from keras.models import load_model
+import mediapipe as mp
+import cv2
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Load the trained model
+model = load_model('../Models/Model_Sign_Language_MNIST.h5')
+
+# Initialize MediaPipe Hands
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+# Open the default camera
+cap = cv2.VideoCapture(0)
+_, frame = cap.read()
+h, w, c = frame.shape
+
+# Define the alphabet letters
+letterpred = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+              'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+while True:
+    # Read a frame from the camera
+    _, frame = cap.read()
+
+    # Process the frame with MediaPipe Hands
+    result = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    hand_landmarks = result.multi_hand_landmarks
+
+    # Draw hand landmarks and rectangles around detected hands
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
+
+            # Calculate bounding box around the hand
+            x_min, y_min, x_max, y_max = w, h, 0, 0
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x < x_min:
+                    x_min = x
+                if x > x_max:
+                    x_max = x
+                if y < y_min:
+                    y_min = y
+                if y > y_max:
+                    y_max = y
+
+            # Draw rectangle around the hand
+            cv2.rectangle(frame, (x_min - 20, y_min - 20),
+                          (x_max + 20, y_max + 20), (0, 255, 0), 2)
+
+            # Crop and preprocess the hand region for gesture recognition
+            analysisframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            analysisframe = analysisframe[y_min -
+                                          20:y_max + 20, x_min - 20:x_max + 20]
+            analysisframe = cv2.resize(analysisframe, (28, 28))
+            analysisframe = analysisframe / 255
+            analysisframe = np.expand_dims(analysisframe, axis=0)
+            analysisframe = np.expand_dims(analysisframe, axis=-1)
+
+            # Make prediction using the model
+            prediction = model.predict(analysisframe)
+            predarray = np.array(prediction[0])
+            letter_prediction_dict = {
+                letterpred[i]: predarray[i] for i in range(len(letterpred))}
+            predarrayordered = sorted(predarray, reverse=True)
+            high1 = predarrayordered[0]
+            high2 = predarrayordered[1]
+            high3 = predarrayordered[2]
+            for key, value in letter_prediction_dict.items():
+                if value == high1:
+                    print("Predicted Character 1: ", key)
+                    print('Confidence 1: ', 100 * value)
+                    #  delete #  for nest 6 line for display top 3 Predicted
+                # elif value == high2:
+                #     print("Predicted Character 2: ", key)
+                #     print('Confidence 2: ', 100 * value)
+                # elif value == high3:
+                #     print("Predicted Character 3: ", key)
+                #     print('Confidence 3: ', 100 * value)
+
+    # Display the frame
+    cv2.imshow("Frame", frame)
+
+    # Check for key press
+    k = cv2.waitKey(1)
+    if k % 256 == 27:  # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k % 256 == 32:  # SPACE pressed
+        time.sleep(5)
+    time.sleep(2)
+
+
+# Release the camera and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ readme.md
@@ -0,0 +1,58 @@
+# Sign Language Recognition
+
+This project aims to detect and recognize hand gestures in real-time using computer vision techniques and machine learning models. Specifically, it focuses on recognizing sign language gestures captured through a webcam.
+
+## Overview
+
+Sign Language Recognition is a field of computer vision and machine learning that focuses on interpreting and understanding sign language gestures. It involves developing algorithms and models capable of recognizing and translating hand movements and gestures into corresponding text or spoken language.
+
+This technology plays a crucial role in enabling communication for individuals who are deaf or hard of hearing, as well as facilitating interaction between people who use different sign languages.
+
+Key components of Sign Language Recognition systems include:
+
+1. **Hand Gesture Detection:** Using computer vision techniques to detect and track the movements of hands and fingers in real-time.
+2. **Feature Extraction:** Identifying and extracting relevant features from the detected hand gestures, such as hand shape, movement trajectory, and finger positions.
+3. **Machine Learning Models:** Training machine learning models, such as deep neural networks, to classify and recognize different sign language gestures based on the extracted features.
+   
+4. **Next Step - Natural Language Processing:** Translating recognized gestures into text or spoken language, allowing for seamless communication between sign language users and non-signers.
+
+Sign Language Recognition technology has the potential to enhance accessibility and inclusion for individuals with hearing impairments in various domains, including education, employment, and social interactions.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+    ```bash
+    git clone https://github.com/jarray-najm/Sign_Language_Recognition.git
+
+2. Open the `Model_Sign_Language_MNIST.ipynb` notebook in Google Colab.
+3. Execute the notebook to train a new model version if needed.
+4. Set up a new project directory on your local machine.
+5. Install the required dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+
+## Usage
+
+1. After installing the dependencies, run the following command to start the application:
+
+    ```bash
+    python CameraHandSign.py
+
+2. Point your webcam towards your hand and perform sign language gestures.
+3. Wait for the results to be displayed in the terminal.
+
+## Acknowledgments
+
+1. The deep learning model used in this project is based on the Sign Language MNIST dataset.
+2. Hand gesture detection is performed using the Mediapipe library.
+
+## Contact
+
+For inquiries or feedback, feel free to reach out via email or whatsApp:
+- Email: najmejarray@gmail.com
+- WhatsApp: +21628259497
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
